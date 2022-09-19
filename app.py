e6e61fb2818fc4f0fbc754cccbd9eb5cf0400643
demon7x <demon7x@demon7xui-MacBookAir.local>
2022-09-19 12:16:46 +0900

    20220919

--- app.py
+++ app.py
@@ -5,22 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    jpg_files = os.listdir('static/assets') 
+    jpg_files = os.listdir('static/assets/qs') 
     index_count = len(jpg_files)   
     return render_template('index.html', index_count=index_count)
 
 
 @app.route('/sketch<index>')
 def sketch(index):
-    index_name = "%02d"%int(index)
-    return render_template('sketch_loop.html',index_name = index_name)
+    index = int(index) + 1
+    index_name = "%03d"%int(index) 
+    return render_template('sketch_two.html',index_name = index_name)
 
 @app.route('/sketch_no<index>')
 def sketch_no(index):
-    index_name = "%02d"%int(index)
+    index_name = "%03d"%int(index)
     return render_template('sketch_NoBG.html',index_name = index_name)
 
 @app.route('/wave<index>')
 def wave(index):
-    index_name = "%02d"%int(index)
+    index_name = "%03d"%int(index)
     return render_template('wave.html',index_name = index_name)
